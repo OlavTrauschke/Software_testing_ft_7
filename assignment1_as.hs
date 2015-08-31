a9e341cac626126ca61edc5fbe922261826b2b7a
AregUva <areg.uvagm@gmail.com>
2015-08-31 14:30:39 +0200

    Exercise 1

--- assignment1_as.hs
+++ assignment1_as.hs
@@ -1,1 +1,7 @@
--- Areg Shahbazian
+-- Areg Shahbazian, Lab 01
+
+lastDigit :: Integer -> Integer
+lastDigit n = read [x]::Integer where (x:xs) = reverse (show n)
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = read (reverse xs) where (x:xs) = reverse (show n)
