c4cf896a0c3dcf56218cc59b0da41393508f82b5
Olav <olav.trauschke@student.uva.nl>
2015-08-31 14:24:20 +0200

    Added dropLastDigit

--- assignment1OCT.hs
+++ assignment1OCT.hs
@@ -2,3 +2,10 @@ import Data.Char
 
 lastDigit :: Integer -> Integer
 lastDigit = toInteger.digitToInt.last.show
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit = read.allButLast.show
+
+allButLast :: [a] -> [a]
+allButLast (x:y:zs) = x:allButLast (y:zs)
+allButLast _ = []
