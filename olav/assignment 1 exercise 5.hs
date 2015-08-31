fa8da3778d83dea0ff83a15889a1ceb27563df39
Olav <olav.trauschke@student.uva.nl>
2015-08-31 15:34:47 +0200

    My implementation of exercise 5

---
+++ olav/assignment 1 exercise 5.hs
@@ -0,0 +1,25 @@
+-- Exercise 1
+lastDigit :: Integer -> Integer
+lastDigit n = rem n 10
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = (div n 10)
+
+-- Exercise 2
+toRevDigits :: Integer -> [Integer]
+toRevDigits n | n > 0     = lastDigit n : toRevDigits (dropLastDigit n)
+              | otherwise = []
+
+-- Exercise 3
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:y:zs) = x:2*y:doubleEveryOther zs
+doubleEveryOther x = x
+
+-- Exercise 4
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = sum(toRevDigits x) + sumDigits xs
+
+-- Exercise 5
+luhn :: Integer -> Bool
+luhn n = mod (sumDigits (doubleEveryOther (toRevDigits n))) 10 == 0
