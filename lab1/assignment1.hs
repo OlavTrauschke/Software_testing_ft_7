55a87fc8e3cf465d663bc50b8a306954c42d2eb8
Olav <olav.trauschke@student.uva.nl>
2015-08-31 15:09:58 +0200

    Beginning exercise 4 and added my version of exercise 3 to the master file

--- lab1/assignment1.hs
+++ lab1/assignment1.hs
@@ -9,3 +9,8 @@ dropLastDigit n = (div n 10)
 toRevDigits :: Integer -> [Integer]
 toRevDigits n | n > 0     = lastDigit n : toRevDigits (dropLastDigit n)
               | otherwise = []
+
+-- Exercise 3
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:y:zs) = x:2*y:doubleEveryOther zs
+doubleEveryOther x = x
--- olav/assignment 1 exercise 3.hs
+++ olav/assignment 1 exercise 3.hs
@@ -1,3 +1,3 @@
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther (x:y:zs) = x:(2*y):(doubleEveryOther zs)
+doubleEveryOther (x:y:zs) = x:2*y:doubleEveryOther zs
 doubleEveryOther x = x
