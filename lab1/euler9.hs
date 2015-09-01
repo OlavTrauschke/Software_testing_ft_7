956308ae437d9ba760e4f611c952fdf825735192
Olav <olav.trauschke@student.uva.nl>
2015-09-01 18:16:29 +0200

    Excluded case where a=0, because 0 is not natural.

--- lab1/euler9.hs
+++ lab1/euler9.hs
@@ -1,2 +1,2 @@
 problem9 = head [ a * b * c | (a, b, c) <- pythagoreanTriplets, a + b + c == 1000]
-    where pythagoreanTriplets = [(a, b, c) | c <- [2..], b <- [1..(c-1)], a <- [0..(b-1)], a^2 + b^2 == c^2]
+    where pythagoreanTriplets = [(a, b, c) | c <- [3..], b <- [2..(c-1)], a <- [1..(b-1)], a^2 + b^2 == c^2]
