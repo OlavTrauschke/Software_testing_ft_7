1a5ddf6b003f48cb6169e7a834a64cff297f6ae0
Olav <olav.trauschke@student.uva.nl>
2015-09-01 18:00:33 +0200

    Small last optimization of my implementation of problem 9 of the Euler project

--- olav/Euler 9.hs
+++ olav/Euler 9.hs
@@ -1,4 +1,4 @@
 pythagoreanTriplet :: Integer
-pythagoreanTriplet = head [a*b*c | c <- [333..500], b <- [1..c], a <- [1..b], a^2+b^2==c^2, a+b+c==1000]
+pythagoreanTriplet = head [a*b*c | c <- [333..500], b <- [1..(c-1)], a <- [1..(b-1)], a^2+b^2==c^2, a+b+c==1000]
 
 --Running this results in 31875000
