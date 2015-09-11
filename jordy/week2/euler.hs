066385774fe45d4db96b0a25e8c7303c778eb9d1
Jordy Heemskerk <jordy.heemskerk@gmail.com>
2015-09-11 10:02:06 +0200

    + Added problem 2 to my euler file + Added getRandomElement, but is not fully functional yet.

--- jordy/week2/euler.hs
+++ jordy/week2/euler.hs
@@ -1,4 +1,11 @@
+import Data.List
+
 problem1 :: Integer
 problem1 = sum [x | x <- [0..999], mod x 3 == 0 || mod x 5 == 0]
 
 problem2 :: Integer
+problem2 = sum (filter (even) (takeWhile (< 4000000) fib))
+
+-- fibonaci sequence generator from Rosetta Code
+fib :: [Integer]
+fib = 1 : 2 : zipWith (+) fib (tail fib)
--- jordy/week2/permutations.hs
+++ jordy/week2/permutations.hs
@@ -5,14 +5,17 @@ import Data.List
 import System.Random
 import Control.Monad
 
+-- Sneaky implementation by Jordy
 isPermutation' :: Eq a => [a] -> [a] -> Bool
 isPermutation' xs ys = elem ys (permutations xs)
 
+-- Custom implementation by Areg
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation [] [] = True
 isPermutation (x:xs) ys = (elem x ys) && (isPermutation xs (delete x ys))
 isPermutation _ _ = False
 
+-- Altered test function from Lecture 2
 testR' :: Int -> Int -> ([Int] -> [Int] -> Bool)
                     -> ([Int] -> [Int] -> Bool -> Bool) -> IO ()
 testR' k n f r = if k == n then print (show n ++ " tests passed")
@@ -24,18 +27,22 @@ testR' k n f r = if k == n then print (show n ++ " tests passed")
                        testR' (k+1) n f r
                   else error ("failed test on: " ++ show xs)
 
+-- Test if the tested function returns the right anwser
 permTest :: Eq a => [a] -> [a] -> Bool -> Bool
 permTest xs ys r = elem xs (permutations ys) == r
 
+-- Altered test function from Lecture 2
 testRel' :: ([Int] -> [Int] -> Bool) -> ([Int] -> [Int] -> Bool -> Bool) -> IO ()
 testRel' f r = testR' 1 100 f r
 
+-- Altered list generator from Lecture 2
 genIntList' :: IO [Int]
 genIntList' = do 
   k <- getRandomInt 20
   n <- getRandomInt 5
   getIntL' k n
 
+-- Altered list generator from Lecture 2
 getIntL' :: Int -> Int -> IO [Int]
 getIntL' _ 0 = return []
 getIntL' k n = do 
@@ -43,10 +50,19 @@ getIntL' k n = do
    xs <- getIntL' k (n-1)
    return (x:xs)
 
+-- Randomly choose either a permutation of the input list or a new random list
 randomPermutation :: IO [Int] -> IO [Int]
 randomPermutation xs = do 
    b <- getRandomInt 1
-   if b==0 then (fmap (last.permutations) xs) else genIntList'
+   if b==0 then getRandomElement (fmap permutations xs) else genIntList'
 
---getRandomElement :: IO [a] -> IO a
---getRandomElement xs = (fmap ((fmap (!!) xs).getRandomInt.length) xs)
+-- Choose a random element from a list (currently not really functioning (index out of bound))
+getRandomElement :: IO [[a]] -> IO [a]
+getRandomElement xs = do
+    l <- fmap length xs
+    print l
+    if l == 0 then return []
+        else do
+            x <- getRandomInt (l-1)
+            print x
+            fmap (!! x) xs
