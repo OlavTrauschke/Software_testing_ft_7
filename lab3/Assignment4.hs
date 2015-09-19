d2ed5cbfcc8080c4e80b4f0d3e4d4d07afab46aa
Jordy Heemskerk <jordy.heemskerk@gmail.com>
2015-09-20 00:18:57 +0200

    Forgot a bit of comment in Assignment4

--- lab3/Assignment4.hs
+++ lab3/Assignment4.hs
@@ -22,7 +22,7 @@ isInCnf (Prop _) = True
 isInCnf (Neg (Prop _)) = True
 -- CNF can be a conjuction, for which all sub forms have to be either a disjunction, or a property, and lastly the disjuntion consists of either properties, or their negation.
 isInCnf (Cnj xs) = checkForDisjunctions xs
-    where -- 
+    where -- recursively search for either more disjunctions, properties, or negation of properties.
         checkForDisjunctions xs = all (\x -> case x of {(Prop _) -> True; (Neg (Prop _)) -> True; (Dsj ys) -> checkForDisjunctions ys; _ -> False}) xs
 -- CNF can also be a disjunction of two properties, or their negation.
 isInCnf (Dsj xs) = length xs <= 2 && all (\x -> case x of {(Prop _) -> True; (Neg (Prop _)) -> True; _ -> False}) xs
