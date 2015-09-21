fd75b83d85b99f96ae2c3d8e2fd452f8a99d1325
Olav <olav.trauschke@student.uva.nl>
2015-09-21 13:23:18 +0200

    Downloaded SetOrd.hs and created a working file for myself

---
+++ olav/Assignment4.hs
@@ -0,0 +1,11 @@
+module Assignment4
+
+where
+
+import Data.List
+import System.Random
+import Test.QuickCheck
+
+{-I spent about fifteen minutes rereading chapter 4, of which I previously had
+to make all the exercises. I have no questions about this chapter.-}
+
