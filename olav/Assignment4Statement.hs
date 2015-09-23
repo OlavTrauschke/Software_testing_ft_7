2927b13ea70a966c0276a7d6505d0d027996bed7
Olav <olav.trauschke@student.uva.nl>
2015-09-23 21:52:20 +0200

    Adapted show and implemented lexical analysis for read
    Changed symbol for assignment to more default "=", added {} around
    bodies of if and else statements,

--- olav/Assignment4Statement.hs
+++ olav/Assignment4Statement.hs
@@ -2,16 +2,19 @@ module Assignment4Statement
 
 where
 
+import Data.Char
 import Data.List
+import Data.String
 import Lecture4
+import Prelude hiding (read)--Get rid of predifined read to implement it self
 
 instance Show Statement where
-  show (Ass v e) =    v ++ " <- " ++ (show e)
-  show (Cond c i e) = "if " ++ (show c) ++ (indent 2 ('\n':show i))
-                        ++ "\nelse" ++ (indent 2 ('\n':show e))
+  show (Ass v e) =    v ++ " = " ++ (show e) ++ ";"
+  show (Cond c i e) = "if " ++ (show c) ++ " {" ++ (indent 2 ('\n':show i))
+                        ++ "\n}" ++ "\nelse {" ++ (indent 2 ('\n':show e)) ++ "\n}"
   show (Seq []) =     ""
-  show (Seq x) =      (intercalate ";\n" (map show x)) ++ ";"
-  show (While c s) = "while " ++ (show c) ++ (indent 2 ('\n':(show s)))
+  show (Seq x) =      intercalate "\n" (map show x)
+  show (While c s) = "while " ++ (show c) ++ " {" ++ (indent 2 ('\n':(show s))) ++ "\n}"
 
 indent :: Int -> String -> String
 indent _ "" = ""
@@ -38,3 +41,57 @@ instance Show Expr where
   show (Add e1 e2) =   (show e1) ++ " + " ++ (show e2)
   show (Subtr e1 e2) = (show e1) ++ " - " ++ (show e2)
   show (Mult e1 e2) =  (show e1) ++ " * " ++ (show e2)
+
+read :: String -> [Token]
+read = tokenize
+
+data Token = AssToken | EOSToken | IfToken | ElseToken | WhileToken | OBToken | CBToken
+           | EqToken | LTToken | GTToken | NgToken | CjToken | DjToken | IntToken Integer
+           | VToken String | AddToken | SubtrToken | MultToken
+           deriving (Show)
+
+tokenize :: String -> [Token]
+tokenize [] =                             []
+tokenize (' ':xs) =                       tokenize xs
+tokenize ('\n':xs) =                      tokenize xs
+tokenize (';':xs) =                       EOSToken:tokenize xs
+tokenize ('i':'f':blank:xs) =             IfToken:tokenize xs
+tokenize ('e':'l':'s':'e':blank:xs) =     ElseToken:tokenize xs
+tokenize ('w':'h':'i':'l':'e':blank:xs) = WhileToken:tokenize xs
+tokenize ('{':xs) =                       OBToken:tokenize xs
+tokenize ('}':xs) =                       CBToken:tokenize xs
+tokenize ('=':'=':xs) =                   EqToken:tokenize xs
+tokenize ('=':xs) =                       AssToken:tokenize xs
+tokenize ('<':xs) =                       LTToken:tokenize xs
+tokenize ('>':xs) =                       GTToken:tokenize xs
+tokenize ('!':xs) =                       NgToken:tokenize xs
+tokenize ('&':xs) =                       CjToken:tokenize xs
+tokenize ('|':xs) =                       DjToken:tokenize xs
+tokenize ('+':xs) =                       AddToken:tokenize xs
+--dash for substraction should always be followed by a space
+tokenize ('-':' ':xs) =                   SubtrToken:tokenize xs
+tokenize ('*':xs) =                       MultToken:tokenize xs
+tokenize ('-':xs) =                       (IntToken (stringToInteger n)):tokenize (xs\\n)
+  where n = '-':getNumbers xs
+tokenize (x:xs)
+  | isDigit x =                           (IntToken (stringToInteger n)):tokenize (xs\\n)
+    --variable names should start with a letter and can not equal "if", "else" or "while"
+  | isLetter x =                          (VToken v):tokenize (xs\\v) 
+    where n = getNumbers (x:xs)
+          v = getChars (x:xs)
+
+getNumbers :: String -> String
+getNumbers [] = []
+getNumbers (x:xs)
+  | isDigit x = x:getNumbers xs
+  | otherwise = []
+
+getChars :: String -> String
+getChars [] = []
+getChars (' ':xs) = []
+getChars (x:xs) = x:getChars xs
+
+stringToInteger :: String -> Integer
+stringToInteger [] = 0
+stringToInteger ('-':xs) = -(stringToInteger xs)
+stringToInteger (x:xs) = (toInteger ((digitToInt x))*10^(length xs))+(stringToInteger xs)
