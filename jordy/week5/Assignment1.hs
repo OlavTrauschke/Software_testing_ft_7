a42c0ee378963fb6dfa83061f87d14a82e691324
Jordy Heemskerk <jordy.heemskerk@gmail.com>
2015-09-29 15:44:11 +0200

    Fixed my first assignment, and added my implementation for assignment 2 and 3. Created file for assignment 4.

--- jordy/week5/Assignment1.hs
+++ jordy/week5/Assignment1.hs
@@ -1,10 +1,36 @@
-module Lab5 where
+module Assignment1 where
 
 import Data.List
 import System.Random
 import Lecture5
 
--- Time taken: 3,5 hours
+{-
+ -
+ - Time taken: 3,5 hours
+ - 
+ - *Assignment1> solveAndShowNrc nrcExample
+ - +---------+---------+---------+
+ - | 4  7  8 | 3  9  2 | 6  1  5 |
+ - |   +-----|--+   +--|-----+   |
+ - | 6 |1  9 | 7| 5 |8 | 3  2| 4 |
+ - | 2 |3  5 | 4| 1 |6 | 9  7| 8 |
+ - +---------+---------+---------+
+ - | 7 |2  6 | 8| 3 |5 | 1  4| 9 |
+ - |   +-----|--+   +--|-----+   |
+ - | 8  9  1 | 6  2  4 | 7  5  3 |
+ - |   +-----|--+   +--|-----+   |
+ - | 3 |5  4 | 9| 7 |1 | 2  8| 6 |
+ - +---------+---------+---------+
+ - | 5 |6  7 | 2| 8 |9 | 4  3| 1 |
+ - | 9 |8  3 | 1| 4 |7 | 5  6| 2 |
+ - |   +-----|--+   +--|-----+   |
+ - | 1  4  2 | 5  6  3 | 8  9  7 |
+ - +---------+---------+---------+
+ - [()]
+ - (0.11 secs, 24259912 bytes)
+ - 
+ -}
+
 
 -- Redifined blocks' to return a list of tuples, where each tuple is a list of rows, columns
 blocks' :: [([Row],[Column])]
